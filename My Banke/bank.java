3194bf8c256f5a9deb9ba15fef1484de0894ceb7
Anilparmarl44 <161117802+Anilparmarl44@users.noreply.github.com>
2024-02-24 14:11:54 +0530

    Add files via upload

---
+++ My Banke/bank.java
@@ -0,0 +1,16 @@
+{
+  "name": "Tecno Bank", 
+  "short_name": "Tecno Bank",
+  "icons": [ { 
+            "src": "https://www.axisbank.com/img/icon192x192.jpeg", 
+            "sizes": "192x192",
+            "type": "image/png" 
+            } ], 
+  "start_url": "/?webapp=1&utm_source=NotifyVisitors&utm_medium=pwa",
+  "orientation": "portrait", 
+  "display": "standalone", 
+  "theme_color": "#ae275f",
+  "background_color": "#ae275f", 
+  "gcm_sender_id": "37327394203",
+  "gcm_user_visible_only": true 
+}
